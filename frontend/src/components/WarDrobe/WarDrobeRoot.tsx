eb3c2ec4b98a8e3d9f630ccf099b6c9a6803842e
Leonardo Zanette <leonardobzanette@gmail.com>
2023-07-11 12:13:19 -0300

    começando o guarda roupa

--- frontend/src/components/WarDrobe/WarDrobeRoot.tsx
+++ frontend/src/components/WarDrobe/WarDrobeRoot.tsx
@@ -3,14 +3,16 @@ import { PlusSquare } from 'lucide-react'
 
 type WarDrobeRootProps = {
   children: ReactNode
-  title: String
+  title: string
 }
 export function WarDrobeRoot({ children, title }: WarDrobeRootProps) {
   return (
     <>
       <div className="flex gap-6">
         <h1 className="text-slate-100">{title}</h1>
-        <PlusSquare size={34} />
+        <button onClick={() => console.log('clicou')}>
+          <PlusSquare size={20} />
+        </button>
       </div>
       <section className="flex flex-wrap gap-3 bg-slate-50">{children}</section>
     </>
